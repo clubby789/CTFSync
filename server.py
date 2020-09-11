33256e780dc826ccd4d1d9c4f56aa48f99e083ac
clubby789 <jamie@hill-daniel.co.uk>
2020-09-11 18:15:22 +0100

    Add cursor sync and initialize files from FS NOTE: Only the first file found will actually be edited. Need to implement a file browser.

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from aiohttp import web
 import socketio
 from diff_match_patch import diff_match_patch
+import threading
+import glob
 
 sio = socketio.AsyncServer()
 app = web.Application()
@@ -11,7 +13,8 @@ dmp = diff_match_patch()
 class Document:
     def __init__(self, filename):
         self.filename = filename
-        self.contents = ""
+        with open(self.filename, 'r') as f:
+            self.contents = f.read()
 
     def apply_patch(self, patch):
         self.contents, err = dmp.patch_apply(patch, self.contents)
@@ -26,6 +29,18 @@ class Document:
 
 
 docs = [Document("example.md")]
+
+
+def save_all_loop():
+    for doc in docs:
+        doc.save()
+    threading.Timer(5, save_all_loop).start()
+
+
+def init_docs():
+    for filename in glob.glob('data/*'):
+        docs.append(Document(filename))
+    save_all_loop()
 
 
 async def index(request):
@@ -43,7 +58,6 @@ async def connect(sid, environ):
 @sio.on('chat message', namespace='/chat')
 async def message(sid, data):
     docs[0].apply_diff(data)
-    docs[0].save()
     await sio.emit('patch', data, namespace='/chat',
                    broadcast=True, skip_sid=sid)
 
@@ -57,4 +71,5 @@ app.router.add_static('/static/', 'static', name='static')
 app.router.add_get('/', index)
 
 if __name__ == '__main__':
+    init_docs()
     web.run_app(app)
