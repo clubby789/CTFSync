b91e8fd8425c3d3b264d5ba58d55e6885fda5d41
clubby789 <jamie@hill-daniel.co.uk>
2020-09-18 20:25:14 +0100

    Add command line option for port

--- __main__.py
+++ __main__.py
@@ -1,11 +1,16 @@
 import os
 
 
-def main():
+def main(port):
     from . import server
-    os.chdir(os.path.dirname(os.path.abspath(__file__))) 
-    server.start_notes()
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    server.start_notes(port=port)
 
 
 if __name__ == "__main__":
-    main()
+    import argparse
+    parser = argparse.ArgumentParser(description='Launch a CTFSync server')
+    parser.add_argument('--port', metavar='port', type=int, const=8080,
+                        nargs='?', help='Port to launch on')
+    args = parser.parse_args()
+    main(args.port)
