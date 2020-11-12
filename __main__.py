e1e417621565ce9c9956703fe640c2d6e2a0f80b
clubby789 <jamie@hill-daniel.co.uk>
2020-11-12 18:13:09 +0000

    Use config from ini

--- __main__.py
+++ __main__.py
@@ -1,16 +1,14 @@
 import os
 
 
-def main(port):
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+def main(port=8080, oauth=False):
     from . import server
     server.start_notes(port=port)
 
 
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description='Launch a CTFSync server')
-    parser.add_argument('--port', metavar='port', type=int, const=8080,
-                        nargs='?', help='Port to launch on')
-    args = parser.parse_args()
-    main(args.port)
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    import configparser
+    config = configparser.ConfigParser()
+    config.read('conf.ini')
+    main(config.getint('MISC', 'port'), config.getboolean('OAUTH', 'enabled'))
--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import glob
 import json
 import os
 
+
 dmp = diff_match_patch()
 sio = socketio.AsyncServer()
 
